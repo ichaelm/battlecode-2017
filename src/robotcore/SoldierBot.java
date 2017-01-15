bba36f84e3dd777ab337bcfd8994c48e341e71ba
Seth Schaffer <qwertbochaffer@gmail.com>
2017-01-14 23:09:06 -0500

    Make attacking work better
    Makes soldiers pick a random archon to attack, see if it's been visited
    and then pick a new one. After visiting all of them, wander. All while
    attacking any enemy found.

--- src/robotcore/SoldierBot.java
+++ src/robotcore/SoldierBot.java
@@ -3,8 +3,15 @@ package robotcore;
 import battlecode.common.*;
 import robotcore.RobotGlobal;
 
-public class SoldierBot extends RobotGlobal {
 
+
+public class SoldierBot extends RobotGlobal {
+	static RobotInfo nearestEnemy = null;
+	static boolean[] visitedArchonLocations = new boolean[enemyInitialArchonLocations.length];
+	static boolean wander = false;
+	static int goCount = 0;
+	static Direction goDir;
+	
     public static void loop() {
         while (true) {
             try {
@@ -21,7 +28,36 @@ public class SoldierBot extends RobotGlobal {
             }
         }
     }
-
+    
+    static int a = (int) (Math.random()*enemyInitialArchonLocations.length);
+    
+ // Makes soldiers explore more archon locations once reaching one and later wander the whole map
+    public static void coordinate() throws GameActionException { 
+    	MapLocation arcLoc = enemyInitialArchonLocations[a];
+    	nearestEnemy = getNearestEnemy();
+    	if (wander) return;
+    	// if I can see the Archon location I'm headed to and there isn't an enemy near it...
+    	if (rc.canSenseLocation(arcLoc) && nearestEnemy == null) { 
+    		rc.setIndicatorLine(myLoc, arcLoc, 222, 222, 222);
+    		visitedArchonLocations[a] = true;
+    		
+    		if (enemyInitialArchonLocations.length == 1) return; // if only one, do nothing
+    		int i = 0;
+    		for (boolean v: visitedArchonLocations) { // find next unvisited location
+    			
+    			if (v) i++;
+    			else {
+    				a = i;
+    				return;
+    			}
+    		}
+    		
+    		System.out.println("All archon locations visited!");
+    		wander = true;			// activate wander mode once Archon locations have been checked
+    	}
+    	
+    }
+    
     public static void turn() throws GameActionException {
         /*
                 int moveMode = 0; // 0 is approach, 1 is kite lumberjack, 2 is run from lumberjack
@@ -65,18 +101,34 @@ public class SoldierBot extends RobotGlobal {
         debugTick(2);
         processNearbyBullets();
         debugTick(3);
-        RobotInfo nearestEnemy = getNearestEnemy();
+        coordinate();
         debugTick(4);
-        int a = 0;
-        a = roundNum >=  1 ? (int) (Math.random()*enemyInitialArchonLocations.length) : a;
-        rc.setIndicatorDot(enemyInitialArchonLocations[a], 255, 0, 255);
-        Direction goDir = myLoc.directionTo(enemyInitialArchonLocations[a]);
-        goDir = rc.canMove(goDir) ? goDir: randomDirection();
+        
+        if (!wander) {
+        	rc.setIndicatorDot(enemyInitialArchonLocations[a], 255, 0, 255);
+            //rc.setIndicatorLine(myLoc, enemyInitialArchonLocations[a], 1, 1, 1);
+        	goDir = myLoc.directionTo(enemyInitialArchonLocations[a]);
+        	goDir = rc.canMove(goDir) ? goDir: randomDirection();
+        }
+        else {
+        	rc.setIndicatorDot(myLoc, 0, 200, 0);
+        	if (goCount < 20 && rc.canMove(goDir)) {
+        		goCount ++;
+        	} 
+        	else {
+        		goCount = 0;
+        		goDir = randomDirection();
+        	}
+        	
+        }
+        debugTick(5);
+        
+        
         if (nearestEnemy != null) {
             goDir = myLoc.directionTo(nearestEnemy.location);
         }
 
-        debugTick(5);
+      
         boolean moved = tryMoveElseLeftRight(goDir);
         debugTick(6);
         if (!moved) {
