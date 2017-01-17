99a2eeb848f13c414503ffe26a205f9bac0cf45a
Michael Schaffer <mls278@case.edu>
2017-01-16 20:28:25 -0500

    Add bots for testing scouts and lumberjacks

---
+++ src/soldierrush1s/RobotPlayer.java
@@ -0,0 +1,45 @@
+package soldierrush1s;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+import robotcore.*;
+
+public strictfp class RobotPlayer {
+    public static void run(RobotController rc) throws GameActionException {
+        try {
+            RobotGlobal.init(rc);
+            RobotGlobal.setInitialBuildQueue1(new RobotType[]{
+                    RobotType.SCOUT
+            });
+            RobotGlobal.setInitialDefaultBuild(RobotType.SOLDIER);
+            RobotGlobal.setGardenerSchedule(RobotGlobal.GardenerSchedule.ONCE_EVERY_N_ROUNDS);
+            RobotGlobal.setGardenerScheduleN(150);
+        } catch (Exception e) {
+            System.out.println("Exception during global init");
+            e.printStackTrace();
+        }
+
+        switch (RobotGlobal.myType) {
+            case ARCHON:
+                ArchonBot.loop();
+                break;
+            case GARDENER:
+                GardenerBot.loop();
+                break;
+            case SOLDIER:
+                SoldierBot.loop();
+                break;
+            case LUMBERJACK:
+                LumberjackBot.loop();
+                break;
+            case SCOUT:
+                ScoutBot.loop();
+                break;
+            case TANK:
+                TankBot.loop();
+                break;
+        }
+	}
+
+}
