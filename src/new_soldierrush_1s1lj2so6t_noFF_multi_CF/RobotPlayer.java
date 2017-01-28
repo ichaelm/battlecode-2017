4ac1351e267cd54dd2f8cc37cf67705686663c17
schlayer <qwertbochaffer@gmail.com>
2017-01-28 14:24:37 -0500

    Testing More

---
+++ src/new_soldierrush_1s1lj2so6t_noFF_multi_CF/RobotPlayer.java
@@ -0,0 +1,62 @@
+package new_soldierrush_1s1lj2so6t_noFF_multi_CF;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+import robotcore.*;
+
+public strictfp class RobotPlayer {
+    public static void run(RobotController rc) throws GameActionException {
+        try {
+            RobotGlobal.init(rc);
+            RobotGlobal.setInitialBuildQueue1(new RobotType[]{
+            		RobotType.SOLDIER,
+            		RobotType.LUMBERJACK,
+            		RobotType.SCOUT,
+            		RobotType.SCOUT
+            });
+            RobotGlobal.setInitialBuildQueue2(new RobotType[]{
+            		RobotType.TANK,
+            		RobotType.TANK,
+                    RobotType.TANK,
+                    RobotType.TANK,
+                    RobotType.TANK,
+                    RobotType.TANK
+            });
+            RobotGlobal.setInitialDefaultBuild(RobotType.SOLDIER);
+            RobotGlobal.setGardenerSchedule(RobotGlobal.GardenerSchedule.ONCE_EVERY_N_ROUNDS);
+            RobotGlobal.setGardenerScheduleN(175);
+            RobotGlobal.friendlyFireOn = false;
+            RobotGlobal.useTriad = true;
+            RobotGlobal.usePentad = true;
+            RobotGlobal.ceaseFire = true;
+            //RobotGlobal.kite = true;
+            
+        } catch (Exception e) {
+            System.out.println("Exception during global init");
+            e.printStackTrace();
+        }
+
+        switch (RobotGlobal.myType) {
+            case ARCHON:
+                ArchonBot.loop();
+                break;
+            case GARDENER:
+                GardenerBot.loop();
+                break;
+            case SOLDIER:
+                SoldierBot.loop();
+                break;
+            case LUMBERJACK:
+                LumberjackBot.loop();
+                break;
+            case SCOUT:
+                ScoutBot.loop();
+                break;
+            case TANK:
+                TankBot.loop();
+                break;
+        }
+	}
+
+}
